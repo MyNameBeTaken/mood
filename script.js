3850150b28361b15193ad73e38e67918fb78ddfb
MyNameBeTaken <Andrewkuo2009@gmail.com>
2025-04-14 14:25:09 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function happy() {
+    document.body.style.backgroundImage = "url('EVA.jpg')";
+}
+
+function sad() {
+    document.body.style.backgroundImage = "url('eva2.jpg')"
+}
+
+function nothing() {
+    document.body.style.backgroundImage = "url('white.jpg')"
+    alert("(*&^%$2_)(<32*&(*^%#$")
+}
+function remove() {
+    document.body.style.backgroundImage = "url('EVA.jpg')"
+}
